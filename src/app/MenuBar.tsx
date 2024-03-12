8f97edf820b5afad54db6f916a6ed41a6ff860ce
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-03-11 23:55:31 -0700

    MenuBar scaffolding

--- src/app/MenuBar.tsx
+++ src/app/MenuBar.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styles from "./MenuBar.module.css";
 
 export default function MenuBar() {
-  return <div>Menu Bar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>Trip Library</div>
+      <div className={styles.filters}>Filters</div>
+      <div>Action buttons</div>
+    </div>
+  );
 }

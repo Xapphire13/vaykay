9e9f52ac2557342aaa5ba77190735ab88b586c6f
LothCat <34076315+lothcat@users.noreply.github.com>
2024-04-17 19:38:10 -0700

    Created static buttons that use BootStrap React icons

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./Header.module.css";
+import PrimaryMenuButton from "./PrimaryMenuButton";
 
 export default function Header() {
   return (
     <header className={styles.container}>
       <h1 className={styles.brand}>Vaykay</h1>
 
-      <div className={styles.content}>Menu</div>
+      <div className={styles.content}>
+        <PrimaryMenuButton />
+      </div>
     </header>
   );
 }
--- src/app/MenuBar.tsx
+++ src/app/MenuBar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./MenuBar.module.css";
+import SearchButton from "./SearchButton";
+import CreateNewTripButton from "./CreateNewTripButton";
 
 export default function MenuBar() {
   return (
     <div className={styles.container}>
       <div className={styles.heading}>Trip Library</div>
       <div className={styles.filters}>Filters</div>
-      <div>Action buttons</div>
+      <div>
+        <SearchButton />
+        <CreateNewTripButton />
+      </div>
     </div>
   );
 }

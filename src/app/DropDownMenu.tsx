acc3129e494ee44a52e1ecd3d2298579cf05f887
LothCat <34076315+lothcat@users.noreply.github.com>
2024-04-18 14:34:04 -0700

    Basic dropdown menu outline

--- src/app/DropDownMenu.tsx
+++ src/app/DropDownMenu.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "./DropDownMenu.module.css";
+import { ChevronDown } from "react-bootstrap-icons";
 
 export default function DropDownMenu() {
   return (
     <>
       <div className={styles.dropdown_menu}>
-        <button className={styles.dropdown_button}>Dropdown</button>
-        <div className={styles.dropdown_content}>
-          <a href="#">Cats</a>
-          <a href="#">Many Dogs</a>
-        </div>
+        <button className={styles.dropdown_button}>
+          Dropdown Choices{" "}
+          <span className={styles.dropdown_chevron}>
+            <ChevronDown />
+          </span>
+        </button>
+        <ul className={styles.dropdown_content}>
+          <li className={styles.dropdown_item}>Cats</li>
+          <li className={styles.dropdown_item}>Many Dogs</li>
+          <li className={styles.dropdown_item}>Pink Lemonade Drink</li>
+          <li className={styles.dropdown_item}>Turtles</li>
+        </ul>
       </div>
     </>
   );

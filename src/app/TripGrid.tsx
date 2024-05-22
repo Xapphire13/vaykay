a78ba12914ee3eb47060a193ed4d84fb07bd07f9
Steven <3392349+Xapphire13@users.noreply.github.com>
2024-05-21 23:15:38 -0700

    Fetch trips from DB (#6)

--- src/app/TripGrid.tsx
+++ src/app/TripGrid.tsx
@@ -1,40 +1,22 @@
 import React from "react";
 import TripCard, { TripCardProps } from "./TripCard";
 import * as styles from "./TripGrid.css";
+import useGetTrips from "./hooks/useGetTrips";
 
-const MOCK_TRIPS: TripCardProps[] = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1583586189949-d9b71613b468?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    location: "Tasmania",
-    travelers: "Emma, Johnny, Alex, Winter",
-    startDate: "Aug-24-2050",
-    endDate: "Sep-15-2050",
-    title: "Tazzy Time",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1590407499953-967e2aa7dbe5?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    location: "France, England, Belguim",
-    travelers: "solo",
-    startDate: "May-8-2028",
-    endDate: "May-28-2028",
-    title: "Europe Solo Trip",
-  },
-];
+export default async function TripGrid() {
+  const trips = await useGetTrips();
 
-export default function TripGrid() {
   return (
     <div className={styles.container}>
-      {MOCK_TRIPS.map((trip, i) => (
+      {trips.map((trip, i) => (
         <TripCard
           key={i}
-          image={trip.image}
+          image="https://images.unsplash.com/photo-1583586189949-d9b71613b468?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           location={trip.location}
-          travelers={trip.travelers}
+          travelers="Bingo and Bluey"
           startDate={trip.startDate}
           endDate={trip.endDate}
-          title={trip.title}
+          title={trip.name} // TODO, rename name -> title
         />
       ))}
     </div>

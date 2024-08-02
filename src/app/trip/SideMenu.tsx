4dfadf9b11cebe371214efe033bbae6532f47254
LothCat <34076315+lothcat@users.noreply.github.com>
2024-08-01 18:35:44 -0700

    Got rid of hr

--- src/app/trip/SideMenu.tsx
+++ src/app/trip/SideMenu.tsx
@@ -21,70 +21,60 @@ export default function SideMenu() {
           <Hospital className={styles.icon} />
         </button>
         <div className={styles.description}>Emergency Numbers</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <AirplaneEngines className={styles.icon} />
         </button>
         <div className={styles.description}>Flights</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <House className={styles.icon} />
         </button>
         <div className={styles.description}>Accommodation</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <BusFront className={styles.icon} />
         </button>
         <div className={styles.description}>Transportation</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <Passport className={styles.icon} />
         </button>
         <div className={styles.description}>Important Docs</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <TicketPerforated className={styles.icon} />
         </button>
         <div className={styles.description}>Booked Activities</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <CupStraw className={styles.icon} />
         </button>
         <div className={styles.description}>Dining</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <Cash className={styles.icon} />
         </button>
         <div className={styles.description}>Cost Breakdown</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <SuitcaseLg className={styles.icon} />
         </button>
         <div className={styles.description}>Packing Checklist</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>
           <FileEarmarkCheckFill className={styles.icon} />
         </button>
         <div className={styles.description}>Itinerary</div>
-        <hr className={styles.line}></hr>
       </div>
       <div className={styles.indvContainer}>
         <button>

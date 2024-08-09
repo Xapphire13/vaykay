411cac2fe3945847989bf659fc61250f8f9fc99e
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-08-09 02:50:55 -0700

    format dates

--- src/app/TripCard.tsx
+++ src/app/TripCard.tsx
@@ -3,24 +3,16 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import * as styles from "./TripCard.css";
 import { ArrowRight } from "react-bootstrap-icons";
+import { DateTime } from "luxon";
+import { Trip } from "./hooks/useGetTrips";
 
 export interface TripCardProps {
   image: string;
-  location: string;
-  travelers: string;
-  startDate: Date;
-  endDate: Date;
-  title: string;
+  trip: Trip;
 }
 
-export default function TripCard({
-  image,
-  location,
-  travelers,
-  startDate,
-  endDate,
-  title,
-}: TripCardProps) {
+export default function TripCard({ image, trip }: TripCardProps) {
+  const { location, name, startDate, endDate } = trip;
   const router = useRouter();
 
   const handleClick = () => {
@@ -32,15 +24,19 @@ export default function TripCard({
       <img src={image} className={styles.cardImg} />
       <div className={styles.details}>
         <div>{location}</div>
-        <div>{travelers}</div>
+        <div>Bingo and Bluey</div>
         <div className={styles.date}>
-          <div>{startDate.toISOString()}</div>
+          <div>
+            {DateTime.fromJSDate(startDate).toLocaleString(DateTime.DATE_MED)}
+          </div>
           <ArrowRight />
-          <div>{endDate.toISOString()}</div>
+          <div>
+            {DateTime.fromJSDate(endDate).toLocaleString(DateTime.DATE_MED)}
+          </div>
         </div>
       </div>
       <div className={styles.title}>
-        <div>{title}</div>
+        <div>{name}</div>
       </div>
     </button>
   );
--- src/app/TripGrid.tsx
+++ src/app/TripGrid.tsx
@@ -12,11 +12,7 @@ export default async function TripGrid() {
         <TripCard
           key={i}
           image="https://images.unsplash.com/photo-1583586189949-d9b71613b468?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          location={trip.location}
-          travelers="Bingo and Bluey"
-          startDate={trip.startDate}
-          endDate={trip.endDate}
-          title={trip.name} // TODO, rename name -> title
+          trip={trip}
         />
       ))}
     </div>

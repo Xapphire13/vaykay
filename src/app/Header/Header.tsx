9a7e3c5626ab76afbf97a6876fec46eda8e2a8cd
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-08-09 00:51:45 -0700

    Make brand name clickable

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppMenuButton from "./AppMenuButton";
 import * as styles from "./Header.css";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <h1 className={styles.brand}>Vaykay</h1>
+      <Link href="/">
+        <h1 className={styles.brand}>Vaykay</h1>
+      </Link>
 
       <div className={styles.content}>
         <AppMenuButton />

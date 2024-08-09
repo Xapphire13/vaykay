987165c504b89431550687268b6dea20e6c689bf
LothCat <34076315+lothcat@users.noreply.github.com>
2024-08-08 19:17:59 -0700

    Seperated tab buttons into thier own file

--- src/app/trip/SideMenu.tsx
+++ src/app/trip/SideMenu.tsx
@@ -12,76 +12,55 @@ import { Cash } from "react-bootstrap-icons";
 import { SuitcaseLg } from "react-bootstrap-icons";
 import { FileEarmarkCheckFill } from "react-bootstrap-icons";
 import { Map } from "react-bootstrap-icons";
+import SideMenuChoice from "./SideMenuChoice";
 
 export default function SideMenu() {
   return (
     <div className={styles.container}>
-      <div className={styles.indvContainer}>
-        <button>
-          <Hospital className={styles.icon} />
-        </button>
-        <div className={styles.description}>Emergency Numbers</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <AirplaneEngines className={styles.icon} />
-        </button>
-        <div className={styles.description}>Flights</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <House className={styles.icon} />
-        </button>
-        <div className={styles.description}>Accommodation</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <BusFront className={styles.icon} />
-        </button>
-        <div className={styles.description}>Transportation</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <Passport className={styles.icon} />
-        </button>
-        <div className={styles.description}>Important Docs</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <TicketPerforated className={styles.icon} />
-        </button>
-        <div className={styles.description}>Booked Activities</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <CupStraw className={styles.icon} />
-        </button>
-        <div className={styles.description}>Dining</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <Cash className={styles.icon} />
-        </button>
-        <div className={styles.description}>Cost Breakdown</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <SuitcaseLg className={styles.icon} />
-        </button>
-        <div className={styles.description}>Packing Checklist</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <FileEarmarkCheckFill className={styles.icon} />
-        </button>
-        <div className={styles.description}>Itinerary</div>
-      </div>
-      <div className={styles.indvContainer}>
-        <button>
-          <Map className={styles.icon} />
-        </button>
-        <div className={styles.description}>Map View</div>
-      </div>
+      <SideMenuChoice
+        icon={<Hospital className={styles.icon} />}
+        iconText="Emergency Numbers"
+      />
+      <SideMenuChoice
+        icon={<AirplaneEngines className={styles.icon} />}
+        iconText="Flights"
+      />
+      <SideMenuChoice
+        icon={<House className={styles.icon} />}
+        iconText="Accommodation"
+      />
+      <SideMenuChoice
+        icon={<BusFront className={styles.icon} />}
+        iconText="Transportation"
+      />
+      <SideMenuChoice
+        icon={<Passport className={styles.icon} />}
+        iconText="Important Docs"
+      />
+      <SideMenuChoice
+        icon={<TicketPerforated className={styles.icon} />}
+        iconText="Booked Activities"
+      />
+      <SideMenuChoice
+        icon={<CupStraw className={styles.icon} />}
+        iconText="Dining"
+      />
+      <SideMenuChoice
+        icon={<Cash className={styles.icon} />}
+        iconText="Cost Breakdown"
+      />
+      <SideMenuChoice
+        icon={<SuitcaseLg className={styles.icon} />}
+        iconText="Packing Checklist"
+      />
+      <SideMenuChoice
+        icon={<FileEarmarkCheckFill className={styles.icon} />}
+        iconText="Itinerary"
+      />
+      <SideMenuChoice
+        icon={<Map className={styles.icon} />}
+        iconText="Map View"
+      />
     </div>
   );
 }

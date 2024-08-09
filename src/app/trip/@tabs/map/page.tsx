cf59bd64e1675ca9ebc4031d72e4b47ca95c536c
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-08-09 01:20:07 -0700

    [wip] Add map

--- src/app/trip/@tabs/map/page.tsx
+++ src/app/trip/@tabs/map/page.tsx
@@ -1,3 +1,12 @@
+import dynamic from "next/dynamic";
+
+const Map = dynamic(
+  () => {
+    return import("./Map");
+  },
+  { ssr: false }
+);
+
 export default function MapPage() {
-  return "Map";
+  return <Map />;
 }

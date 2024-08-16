5d2fde4c2b8f06664661cd4dab87992c06c4f469
LothCat <34076315+lothcat@users.noreply.github.com>
2024-08-15 22:09:13 -0700

    Created emergency contact table

--- src/app/trip/[id]/@tabs/numbers/page.tsx
+++ src/app/trip/[id]/@tabs/numbers/page.tsx
@@ -1,5 +1,15 @@
-import TravelerEmerContact from "@/app/trip/emergency/TravelerEmerContact";
+import TravelerEmergencyContactTable, {
+  TravelerEmerContact,
+} from "@/app/trip/emergency/TravelerEmergencyContactTable";
+
+const travelerList: TravelerEmerContact[] = [
+  {
+    travelerName: "Lisa Frank",
+    emergencyContactName: "Veronica Rosemary De La Fromage",
+    eCPhoneNumber: "+1-555-555-5555",
+  },
+];
 
 export default function NumbersPage() {
-  return <TravelerEmerContact />;
+  return <TravelerEmergencyContactTable travelers={travelerList} />;
 }

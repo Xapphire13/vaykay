5758e951a07ac7bfed179125ee90a0fdb69131be
Xapphire13 <3392349+Xapphire13@users.noreply.github.com>
2024-08-30 00:55:50 -0700

    redirect to first tab when navigating to trip

--- src/app/trip/[id]/@tabs/default.tsx
+++ src/app/trip/[id]/@tabs/default.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Default() {
+  const router = useRouter();
+  const params = useParams();
+
+  useEffect(() => {
+    router.replace(`./${params.id}/numbers`);
+  }, [params.id, router]);
+
   return null;
 }
